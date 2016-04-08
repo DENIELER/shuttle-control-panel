cf7bdcebae4a24f3eecd6975234598050307951f
Даниил Остапенко <danielostapenko@gmail.com>
2016-04-08 16:02:24 +0300

    Get server ip function add

--- server/control-site/server.js
+++ server/control-site/server.js
@@ -9,18 +9,38 @@ app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
 
+var ip = getip();
+
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
 
-	var ip = req.headers['x-forwarded-for'] || 
-	     req.connection.remoteAddress || 
-	     req.socket.remoteAddress ||
-	     req.connection.socket.remoteAddress;
-
-	var ipArr = ip.split(':');
-
-  	res.render('index.ejs', { ip: ipArr[ipArr.length - 1] });
+	res.render('index.ejs', { ip: ip[1] });
 });
 
 http.createServer(app).listen(8080);
+
+function getip () {
+	var
+    // Local ip address that we're trying to calculate
+    address = []
+    // Provides a few basic operating-system related utility functions (built-in)
+    ,os = require('os')
+    // Network interfaces
+    ,ifaces = os.networkInterfaces();
+
+	// Iterate over interfaces ...
+	for (var dev in ifaces) {
+
+	    // ... and find the one that matches the criteria
+	    var iface = ifaces[dev].filter(function(details) {
+	        return details.family === 'IPv4' && details.internal === false;
+	    });
+
+	    if(iface.length > 0) {
+	      address.push(iface[0].address);
+	 	}
+	}
+
+	return address;
+}
