0addf710522ed8bbbc8061cf57fab01f5b081618
Даниил Остапенко <danielostapenko@gmail.com>
2016-04-08 15:36:49 +0300

    Fix ip address

--- server/control-site/server.js
+++ server/control-site/server.js
@@ -1,3 +1,16 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.set('view engine', 'ejs');
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+  res.render('index.ejs', { ip: '192.168.0.101' });
+});
+
+http.createServer(app).listen(8080);
